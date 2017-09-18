306740010ba53a8b1a6c4f03abd3b20495c2fc19
rskull <info@rskull.com>
2017-09-19 01:51:46 +0900

    example

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import glamorous, { View, Text } from 'glamorous-native'
+import styled from 'styled-components/native'
 
-export default class App extends React.Component {
+const StyledText = styled.Text`
+  fontSize: 20;
+  color: red;
+`
+
+const GlamorousText = glamorous.text({
+  fontSize: 20,
+  color: 'red'
+})
+
+const MyText = glamorous.text(
+  {
+    fontSize: 20,
+    color: 'red'
+  },
+  (props) => ({
+    fontWeight: props.noBold ? 'normal' : 'bold'
+  })
+)
+
+const MyTextComp = glamorous(MyText)({ fontStyle: 'italic' })
+
+export default class Example extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View margin={20}>
+        <Text color="red" fontSize={20}>
+          glamorous Text!!
+        </Text>
+        <GlamorousText>
+          glamorous text2!!
+        </GlamorousText>
+        <StyledText>
+          styled-components Text!!
+        </StyledText>
+        <MyText>
+          MyText bold!
+        </MyText>
+        <MyText noBold>
+          MyText noBold!
+        </MyText>
+        <MyTextComp>
+          My text comp!
+        </MyTextComp>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
